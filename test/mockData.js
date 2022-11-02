fc828e7522ec738f05d394183f83052df1774dda
KarenthCruz <spoonecologic@gmail.com>
2022-11-02 09:21:55 -0500

    working in unit test

--- test/mockData.js
+++ test/mockData.js
@@ -1,23 +1,21 @@
-const dataForMock = {
+const dataMock = {
     pathFile: 'testDirectory',
-    stats: { Total: 2, Unique: 2 },
-    statsAndValidate: { Total: 4, Unique: 4, Broken: 0 },
     validateFalse: [
         {
             href: 'https://nodejs.org/es/',
             text: 'Node.js',
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
-          },
-          {
+        },
+        {
             href: 'https://developers.google.com/v8/',
             text: 'motor de JavaScript V8 de Chrome',
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
-          },
-          {
+        },
+        {
             href: 'https://developers.guugle.com/v8/',
             text: 'motor de JavaScript V8 de Chrome',
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
-          }
+        }
     ],
     validateTrue: [
         {
@@ -26,22 +24,25 @@ const dataForMock = {
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
             status: 200,
             txt: 'Ok'
-          },
-          {
+        },
+        {
             href: 'https://developers.google.com/v8/',
             text: 'motor de JavaScript V8 de Chrome',
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
             status: 200,
             txt: 'Ok'
-          },
-          {
+        },
+        {
             href: 'https://developers.guugle.com/v8/',
             text: 'motor de JavaScript V8 de Chrome',
             file: '/Users/dana/Documents/Dana 2022/Laboratoria/BOG005-md-links/testDirectory/README.md',
             status: 'No hay respuesta del servidor',
             txt: 'Fail'
-          }
+        },
     ],
+        stats: { Total: 3, Unique: 3 },
+        statsAndValidate: { Total: 3, Unique: 3, Broken: 1 },
+    
 }
 
-module.exports = { dataForMock };
+module.exports = { dataMock };
